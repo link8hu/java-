d7897b48f68cb2a9e79c4731d7e8da3c278b748d
Mr.Hu <tust.hu@qq.com>
2018-11-17 21:58:27 +0800

    装饰者模式

---
+++ ModelOfDesign/Singleton/src/Singleton.java
@@ -0,0 +1,17 @@
+public class Singleton {
+    private  static  final Singleton sing =new Singleton();
+    private Singleton(){
+        System.out.println("helloWorld");
+    }
+    public static Singleton getInstance(){
+        return sing;
+    }
+}
+ class main{
+    public static void main(String[] args) {
+        Singleton s1=Singleton.getInstance();
+        Singleton s2=Singleton.getInstance();
+        if(s1==s2) System.out.println("同一个对象");
+        else System.out.println("两个对象");
+    }
+}
