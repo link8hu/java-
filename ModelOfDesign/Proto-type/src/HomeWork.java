06c9ec30f7131300831f5ee7f8e88c6f01c9b1de
Mr.Hu <tust.hu@qq.com>
2018-11-18 22:18:49 +0800

    原型模式

--- ModelOfDesign/Proto-type/src/HomeWork.java
+++ ModelOfDesign/Proto-type/src/HomeWork.java
@@ -1,20 +1,42 @@
+import java.io.*;
+
 /**
  * @program: ModelOfDesign
  * @description: 等待克隆对象
  * @author: Mr.Hu
  * @create: 2018-11-18 21:06
  */
-public class HomeWork implements Cloneable{
+public class HomeWork implements Cloneable , Serializable{
+    private static final long serialVersionUID = 7362717931684462047L;
     String couse;             //作业名
     int time;                 //作业提交时间
 
 
-/***实现克隆方法******/
+    /***实现浅克隆方法******/
     public HomeWork clone(){
         HomeWork a = null;
         try {
             a = (HomeWork) super.clone();
         } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return a;
+    }
+    /***实现深克隆方法******/
+    public HomeWork DeepClone(){
+        HomeWork a = null;
+        try {
+            /**将对象序列化成流 而序列化时原对象仍在jVM中，所以利用这个特性可以实现深度拷贝**/
+            ByteArrayOutputStream bao = new ByteArrayOutputStream();
+            ObjectOutputStream oot =new ObjectOutputStream(bao);
+            oot.writeObject(this);
+            //将流序列化成对象
+            ByteArrayInputStream bai = new ByteArrayInputStream(bao.toByteArray());
+            ObjectInputStream obi = new ObjectInputStream(bai);
+            a = (HomeWork) obi.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         return a;
--- ModelOfDesign/Proto-type/src/Proto.java
+++ ModelOfDesign/Proto-type/src/Proto.java
@@ -8,17 +8,13 @@
 /**
  * 拓展cloneable() 实现的是浅拷贝
  * 实现深克隆：1.覆盖object的clone()方法（如果引用类型里面还包含很多引用类型，或者内层引用类型的类里面又包含引用类型，使用clone方法就会很麻烦。这时我们可以用序列化的方式来实现对象的深克隆。）
- *           2.使用序列化的方式
+ *           2.使用序列化的方式  （序列化就是将对象写到流的过程，写到流中的对象是原有对象的一个拷贝，而原对象仍然存在于内存中。通过序列化实现的拷贝不仅可以复制对象本身，而且可以复制其引用的成员对象，因此通过序列化将对象写到一个流中，再从流里将其读出来，可以实现深克隆。需要注意的是能够实现序列化的对象其类必须实现Serializable接口，否则无法实现序列化操作。）
  *           Java语言提供的Cloneable接口和Serializable接口的代码非常简单，它们都是空接口，这种空接口也称为标识接口，标识接口中没有任何方法的定义，其作用是告诉JRE这些接口的实现类是否具有某个功能，如是否支持克隆、是否支持序列化等。
  */
 public class Proto {
-    public static void main(String[] args) {
-        HomeWork homeWork1 = new HomeWork();
-        homeWork1.setCouse("高数");
-        homeWork1.setTime(1);
+    private void test(HomeWork homeWork1,HomeWork homeWork2){
         System.out.println("--------homework1-------");
         System.out.println(homeWork1.toString());
-        HomeWork homeWork2 =homeWork1.clone();
         System.out.println("--------homework2-------");
         System.out.println(homeWork2.toString());
         System.out.println("接下来开始判断");
@@ -26,6 +22,16 @@ public class Proto {
         System.out.println(homeWork1 == homeWork2);
         System.out.print("判断是否是浅拷贝：");
         System.out.println(homeWork1.couse == homeWork2.couse);
-
+    }
+    public static void main(String[] args) {
+        Proto proto =new Proto();
+        HomeWork homeWork = new HomeWork();
+        homeWork.setCouse("高数");
+        homeWork.setTime(1);
+        /**进行浅深克隆****/
+        HomeWork homeWork1 =homeWork.clone();
+        HomeWork homeWork2 =homeWork.DeepClone();
+        proto.test(homeWork,homeWork1);
+        proto.test(homeWork,homeWork2);
     }
 }
